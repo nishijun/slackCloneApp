29fdd988306c16a63f12aff421affc4999a9f492
Junya Nishiwaki <junya4318629@yahoo.co.jp>
2020-02-12 23:32:34 +0800

    a

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="input-container">
-    <textarea v-model='text' @keydown.enter='addMessage'></textarea>
+    <textarea v-model='text' @keydown.enter='addMessage' @click='openLoginModal'></textarea>
+    <el-dialog title="" :visible.sync="dialogVisible" width="30%">
+      <div class="image-container">
+       <img src="~/assets/signin.png" @click='login'/>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { db } from '~/plugins/firebase.js';
+import { db, firebase } from '~/plugins/firebase.js';
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 export default {
   data() {
     return {
+      dialogVisible: false,
       text: null
     };
   },
   methods: {
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+        console.log(user);
+        this.dialogVisible = false;
+      }).catch(error => {
+        window.alert(error);
+      });
+    },
+    openLoginModal () {
+      this.dialogVisible = true;
+    },
     addMessage(event) {
       if (this.keyDownedForJPConversion(event)) {return;}
       const channelId = this.$route.params.id;
@@ -37,5 +61,15 @@ export default {
 textarea {
  width: 100%;
  height: 100%;
+}
+
+.image-container {
+ display: flex;
+ justify-content: center;
+}
+
+img {
+ width: 70%;
+ cursor: pointer;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <button @click="login">ログイン</button>
   </div>
 </template>
 
@@ -9,16 +8,6 @@ import { firebase } from '~/plugins/firebase.js';
 
 export default {
   methods: {
-    login() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
-        .then(result => {
-          const user = result.user;
-          console.log(user);
-        }).catch(error => {
-          window.alert(error);
-        });
-    }
   }
 }
 </script>
